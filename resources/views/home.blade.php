b6790cf4d55fe3135625415f7f68d4f449ed3f3e
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2024-08-19 14:02:38 +0330

    add icon home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
 
 <head>
     <meta charset="utf-8">
@@ -12,17 +12,20 @@
     <meta name="theme-color" content="#ffffff">
     <title>منوی آنلاین </title>
 
-    <link rel="stylesheet" href="{{ asset('frontend_file/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend_file/assets/css/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend_file/assets/css/bootstrap/bootstrap.rtl.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend_file/assets/plugin/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend_file/assets/css/style.css') }}">
 
 </head>
 
 <body class="alart aleart-sucsess">
+    @include('layouts_master.chackscrine')
     <div class="logo"><img src="{{ asset('frontend_file/assets/img/logo.png') }}" alt="logo site"></div>
     <ul class="menu">
         <li class="menu-item"><a href="{{route('cv')}}">
-            <span class="link-icon">
-
+            <span class="link-icon" active>
+                <i class="fas fa-file" active></i>
             </span>
             <span class="link-text">
                 رزومه موسس
@@ -30,14 +33,14 @@
         </a></li>
         <li class="menu-item"><a href="{{route('about')}}">
             <span class="link-icon">
-
+                <i class="fa-solid fa-users"></i>
             </span>
             <span class="link-text">
                 درباره ما
             </span>
            </a></li>
         <li class="menu-item"><a href="{{route('service')}}">  <span class="link-icon">
-
+            <i class="fa-solid fa-bell-concierge"></i>
         </span>
         <span class="link-text">
             خدمات
@@ -45,7 +48,7 @@
         <li class="menu-item">
             <a href="{{route('branches')}}">
             <span class="link-icon">
-
+                <i class="fas fa-building"></i>
             </span>
             <span class="link-text">
                 شعب
@@ -53,11 +56,18 @@
         </a></li>
     </ul>
 
-    <footer>
+    <footer class="d-grid">
         <div>
             <a href="www.edutsn.com">www.edutsn.com</a>
         </div>
-
+        <div>
+            <ul class="menu-footer">
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                <li><a href=""><i class="fab fa-whatsapp"></i></a></li>
+                <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href=""><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </div>
     </footer>
 </body>
 
