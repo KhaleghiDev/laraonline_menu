6eee2a64e8b8d9e600df366922612760ec9ccfa8
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2024-08-19 15:48:13 +0330

    add componet branchesInput and branches

---
+++ resources/views/components/branches-input.blade.php
@@ -0,0 +1,13 @@
+<div>
+   <span>شعبه تهران :</span>
+   <br>
+   <span class="diescription">
+    تهران -جنب سفارت عراق
+   </span>
+   <br>
+   <span>شعبه مشهد :</span>
+   <br>
+   <span class="diescription">
+        مشهد - میدان شریعتی به سمت احمد آباد 1 جنب بانک دی طبقه اول واحد 3 
+   </span>
+</div>
---
+++ resources/views/components/branches.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <ul class="menu">
+        <li class="menu-item"><a href="{{ route('branchesOutput') }}">
+                <span class="link-icon" >
+                    <i class="fa-solid fa-maximize"></i>
+                </span>
+                <span class="link-text">
+                    شعبه های ما در خارج کشور
+                </span>
+            </a>
+        </li>
+        <li class="menu-item"><a href="{{ route('branchesInput') }}">
+                <span class="link-icon">
+                    <i class="fa-solid fa-minimize"></i>
+                </span>
+                <span class="link-text">
+                    شعبه های ما در داخل کشور
+                </span>
+            </a>
+        </li>
+</div>
