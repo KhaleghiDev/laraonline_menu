b00ad83d08dbec4098df514d70d568a103041399
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2024-09-07 15:14:18 +0330

    add map in project

--- resources/views/components/branchesInput.blade.php
+++ resources/views/components/branchesInput.blade.php
@@ -1,25 +1,34 @@
 <div>
     <div class="container">
-    <div class="row">
-        <div class="box-about">
-            <div class="box-title mb-3">
-                <i class="fa-solid fa-location-dot"></i>
-                         <h3 class="text-white" > شعبه تهران :</h3>
-            </div>
-            <p class="box-white">
-                تهران -جنب سفارت عراق
+        <div class="row">
+            <div class="box-about">
+                <div class="box-title mb-3">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <h3 class="text-white"> شعبه تهران :</h3>
+                </div>
+                <p class="box-white">
+                    تهران -جنب سفارت عراق
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.644663394716!2d51.40342537934569!3d35.7103609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e01146d37532b%3A0xbf20dca85b509407!2sEmbassy%20of%20The%20Republic%20of%20Iraq!5e0!3m2!1sen!2s!4v1725707486800!5m2!1sen!2s"
+                        width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </p>
 
-            </p>
-        </div>
-
-        <div class="box-about">
-            <div class="box-title mb-3">
-                <i class="fa-solid fa-location-dot"></i>
-                <h3 class="text-white"> شعبه مشهد :</h3>
             </div>
-            <p class="box-white">
-                مشهد - میدان شریعتی به سمت احمد آباد 1 جنب بانک دی طبقه اول واحد 3
-            </p>
+
+            <div class="box-about">
+                <div class="box-title mb-3">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <h3 class="text-white"> شعبه مشهد :</h3>
+                </div>
+                <p class="box-white">
+                    مشهد - میدان شریعتی به سمت احمد آباد 1 جنب بانک دی طبقه اول واحد 3
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3215.841906252864!2d59.58319821162683!3d36.291885696072704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f6c91c262930095%3A0x6c932661d2e2451c!2sTolo%20Safiran%20Noor%20Company!5e0!3m2!1sen!2s!4v1725707152232!5m2!1sen!2s"
+                        width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </p>
+
+            </div>
         </div>
     </div>
-</div>
--- resources/views/components/branchesOutput.blade.php
+++ resources/views/components/branchesOutput.blade.php
@@ -8,6 +8,10 @@
                 </div>
                 <p class="box-white">
                     کابل - پل سوخته - دکان سوم
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d451.727772510736!2d69.11638783685717!3d34.50567918697553!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38d169000d281829%3A0xbf7ddf4bf7c6e4f7!2zUHVsIGUgc29raHRhINm-2YQg2LPZiNiu2KrZhw!5e0!3m2!1sen!2sus!4v1725707950392!5m2!1sen!2sus"
+                        width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
 
                 </p>
             </div>
